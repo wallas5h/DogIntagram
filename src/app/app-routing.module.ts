f00a022f0ce741092b08bfdb69235996e698073d
wallas5h@gmail.com <wallas5h@gmail.com>
2022-10-14 22:23:02 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { DogsComponent } from "./dogs/dogs.component";
 
-const routes: Routes = [];
+const routes: Routes = [{ path: "", component: DogsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
