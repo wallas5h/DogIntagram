f01349dfc9fa184ed085ce27ef58e2cee2ccfeb3
wallas5h@gmail.com <wallas5h@gmail.com>
2022-10-14 22:23:20 +0200

    create dogs module and component

---
+++ src/app/dogs/dogs.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { DogsComponent } from "./dogs.component";
+
+@NgModule({
+  declarations: [DogsComponent],
+  imports: [CommonModule],
+})
+export class DogsModule {}
