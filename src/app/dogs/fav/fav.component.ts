5d2a864779b86d138f422da1f6719d7e46634cfe
wallas5h@gmail.com <wallas5h@gmail.com>
2022-10-15 16:29:43 +0200

    add fav component

--- src/app/dogs/fav/fav.component.ts
+++ src/app/dogs/fav/fav.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FavService } from "src/app/fav/fav.service";
 
 @Component({
-  selector: 'app-fav',
-  templateUrl: './fav.component.html',
-  styleUrls: ['./fav.component.scss']
+  selector: "app-fav",
+  templateUrl: "./fav.component.html",
+  styleUrls: ["./fav.component.scss"],
 })
 export class FavComponent implements OnInit {
+  constructor(private favService: FavService) {}
 
-  constructor() { }
+  favourites: string[] = this.favService.getFavourites();
+  apiUrl = "https://dog.ceo/api";
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  async loadImage(selectedBreed: string) {
+    let breed: string = "";
+
+    if (selectedBreed.includes(" ")) {
+      breed = selectedBreed.replace(" ", "/");
+    } else {
+      breed = selectedBreed;
+    }
+
+    const image = await this.getRandomImageByBreed(breed);
+
+    console.log(image);
+
+    return;
   }
 
+  getRandomImageByBreed(breed: string) {
+    return fetch(`${this.apiUrl}/breed/${breed}/images/random`)
+      .then((resp) => resp.json())
+      .then((data) => data.message)
+      .then((img) => img);
+  }
 }
