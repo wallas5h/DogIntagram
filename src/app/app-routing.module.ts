0a58f7698d4556a6b5555c5c12dd54713c1c30f8
wallas5h@gmail.com <wallas5h@gmail.com>
2022-10-15 15:07:36 +0200

    update routing, add fav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { DogsComponent } from "./dogs/dogs.component";
+import { FavComponent } from "./dogs/fav/fav.component";
 
-const routes: Routes = [{ path: "", component: DogsComponent }];
+const routes: Routes = [
+  { path: "", component: DogsComponent },
+  { path: "favourites", component: FavComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
